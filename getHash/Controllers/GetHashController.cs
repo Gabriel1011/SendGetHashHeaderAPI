454850c3b138f112146f8e47eca02cf4cf9cebe9
Gabriel Rodrigues de Almeida <G.ALMEIDA@btp.com.br>
2019-10-09 11:13:35 -0300

    - push

---
+++ getHash/Controllers/GetHashController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace getHash.Controllers
+{
+    [Route("getHash")]
+    [ApiController]
+    public class GetHashController : ControllerBase
+    {
+        private readonly IHttpContextAccessor httpContext;
+
+        private readonly IHttpContextFactory context;
+
+        public GetHashController(IHttpContextAccessor httpContext, IHttpContextFactory context)
+        {
+            this.httpContext = httpContext;
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHash()
+        {
+            string hash = httpContext?.HttpContext?.Request?.Headers["hash"].ToString();
+            return Ok();
+        }
+    }
+}
---
+++ sendHash/Controllers/SendHashController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sendHash.Controllers
+{
+    [Route("sendHash")]
+    [ApiController]
+    public class SendHashController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> SendHash()
+        {
+            var hashcode = Guid.NewGuid().ToString();
+            string url = "https://localhost:44302/getHash";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/html"));
+                client.DefaultRequestHeaders.Add("Hash", hashcode);
+
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                string responsestr = "";
+                if (response.IsSuccessStatusCode)
+                {
+                    responsestr = await response.Content.ReadAsStringAsync();
+
+                    return Json(new { success = true, response = responsestr, hash = hashcode });
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
